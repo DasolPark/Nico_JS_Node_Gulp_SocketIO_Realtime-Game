4ccb5c33f41236200ac67e965b9ac346c9c08827
Dasol Park <dasol.park615@gmail.com>
2019-09-30 20:54:56 +0900

    1-4. Setting Up SocketIO

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import socketIO from "socket.io";
 const PORT = 4000;
 const app = express();
 
-app.set("iew engine", "pug");
+app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 
 app.use(express.static(join(__dirname, "static")));
@@ -13,6 +13,7 @@ app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
 
 const handleListening = () =>
-  console.log(`Server running: http://localhost:${PORT}`);
+  console.log(`✅ Server running: http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening);
+const server = app.listen(PORT, handleListening);
+const io = socketIO(server);
