580ffbc0fefe979debf22d3f19e14093307cc141
Dasol Park <dasol.park615@gmail.com>
2019-09-30 20:25:22 +0900

    1-2. ESLint

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import { join } from 'path';
-import socketIO from 'socket.io';
+import express from "express";
+import { join } from "path";
+import socketIO from "socket.io";
 
 const PORT = 4000;
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', join(__dirname, 'views'));
+app.set("iew engine", "pug");
+app.set("views", join(__dirname, "views"));
 
-app.use(express.static(join(__dirname, 'static')));
+app.use(express.static(join(__dirname, "static")));
 
-app.get('/', (req, res) => res.render('home'));
+app.get("/", (req, res) => res.render("home"));
 
 const handleListening = () =>
   console.log(`Server running: http://localhost:${PORT}`);
